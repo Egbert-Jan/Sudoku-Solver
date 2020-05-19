d156abb504ad0718ad7332e6505720605bc5737e
Egbert-Jan <ejterpstra@icloud.com>
2020-05-19 12:26:50 +0200

    Initial Commit

---
+++ Sudoku Solver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Sudoku Solver
+//
+//  Created by Egbert-Jan Terpstra on 19/05/2020.
+//  Copyright © 2020 Egbert-Jan Terpstra. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
